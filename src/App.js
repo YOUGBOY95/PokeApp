b5e2cf3e3ff9543dddb619893d3a4282e298fbe7
Nathan <86224840+YOUGBOY95@users.noreply.github.com>
2022-10-21 14:15:14 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from './Components/Main';
+import './Components/style.css'
+function App() {
+  return (
+    <>
+       <header className="pokemon-header">PokeApp</header>
+      <Main/>
+      
+    </>
+  );
+}
+
+export default App;
